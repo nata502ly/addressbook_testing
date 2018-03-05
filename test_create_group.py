b0d85a2a642c2ab377e9494bde8f1dec643498ce
Olena Piankova <el.piankova@gmail.com>
2018-03-05 17:44:53 +0200

    Exported Test from Katalon Recorder. We recordered login, created group, logout steps

---
+++ test_create_group.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class TestCreateGroup(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_create_group(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/index.php")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath(u"//input[@value='Войти']").click()
+        driver.find_element_by_link_text(u"Группы").click()
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys("new name")
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys("new header")
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys("new footer")
+        driver.find_element_by_name("submit").click()
+        driver.find_element_by_link_text("group page").click()
+        driver.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
+        try:
+            self.assertEqual("1123new name",
+                             driver.find_element_by_xpath("//form[@action='/addressbook/group.php']").text)
+        except AssertionError as e:
+            self.verificationErrors.append(str(e))
+        driver.find_element_by_link_text(u"Выйти").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
