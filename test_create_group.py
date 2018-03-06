2f375e25b24aae25cf00df63f6f70362ea5a41f3
Olena Piankova <el.piankova@gmail.com>
2018-03-06 11:56:19 +0200

    Identified individual steps by comments

--- test_create_group.py
+++ test_create_group.py
@@ -11,10 +11,11 @@ class TestCreateGroup(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.base_url = "http://localhost/addressbook/"
+        self.driver.get(self.base_url)
 
     def test_create_group(self):
         driver = self.driver
-        driver.get(self.base_url)
+        # Login (precondition)
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
@@ -23,7 +24,10 @@ class TestCreateGroup(unittest.TestCase):
         driver.find_element_by_css_selector('#LoginForm > input[type="submit"]').click()
         # Open group page
         driver.find_element_by_css_selector('#nav > ul > li.admin > a').click()
+        # Creating group:
+        # 1. Initialization group create
         driver.find_element_by_name("new").click()
+        # 2. Fill form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
         driver.find_element_by_name("group_name").send_keys("new name")
@@ -33,8 +37,11 @@ class TestCreateGroup(unittest.TestCase):
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys("new footer")
+        # 3. Submit group
         driver.find_element_by_name("submit").click()
+        # Return to group page
         driver.find_element_by_link_text("group page").click()
+        # Logout (postcondition)
         driver.find_element_by_css_selector("form[name=logout] > a").click()
 
     def is_element_present(self, how, what):
