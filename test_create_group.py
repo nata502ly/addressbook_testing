2c5d7ef673975c74cd1f9c8220f3ebe51fa4e996
Olena Piankova <el.piankova@gmail.com>
2018-03-07 11:44:21 +0200

    Extracted steps-methods

--- test_create_group.py
+++ test_create_group.py
@@ -16,15 +16,21 @@ class TestCreateGroup(unittest.TestCase):
     def test_create_group(self):
         driver = self.driver
         # Login (precondition)
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_css_selector('#LoginForm > input[type="submit"]').click()
+        self.login()
         # Open group page
-        driver.find_element_by_css_selector('#nav > ul > li.admin > a').click()
+        self.open_group_page()
         # Creating group:
+        self.create_group()
+        # Return to group page
+        self.return_to_group_page()
+        # Logout (postcondition)
+        self.logout()
+
+    def return_to_group_page(self):
+        self.driver.find_element_by_link_text("group page").click()
+
+    def create_group(self):
+        driver = self.driver
         # 1. Initialization group create
         driver.find_element_by_name("new").click()
         # 2. Fill form
@@ -39,10 +45,21 @@ class TestCreateGroup(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys("new footer")
         # 3. Submit group
         driver.find_element_by_name("submit").click()
-        # Return to group page
-        driver.find_element_by_link_text("group page").click()
-        # Logout (postcondition)
-        driver.find_element_by_css_selector("form[name=logout] > a").click()
+
+    def logout(self):
+        self.driver.find_element_by_css_selector("form[name=logout] > a").click()
+
+    def open_group_page(self):
+        self.driver.find_element_by_css_selector('#nav > ul > li.admin > a').click()
+
+    def login(self):
+        driver = self.driver
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_css_selector('#LoginForm > input[type="submit"]').click()
 
     def is_element_present(self, how, what):
         try:
