0fdd0ed115c18ea04701703f2b9691fa764201e1
Olena Piankova <el.piankova@gmail.com>
2018-03-07 12:04:50 +0200

    Extracted high-level class for interaction with our App

---
+++ fixtures/addressbook_app.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+
+
+class AddressbookApp:
+    def __init__(self, base_url="http://localhost/addressbook"):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.base_url = base_url
+        self.driver.get(self.base_url)
+
+    def login(self, username, password):
+        driver = self.driver
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_css_selector('#LoginForm > input[type="submit"]').click()
+
+    def logout(self):
+        self.driver.find_element_by_css_selector("form[name=logout] > a").click()
+
+    def open_group_page(self):
+        self.driver.find_element_by_css_selector('#nav > ul > li.admin > a').click()
+
+    def return_to_group_page(self):
+        self.driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, group_name, group_header, group_footer):
+        driver = self.driver
+        # 1. Initialization group create
+        driver.find_element_by_name("new").click()
+        # 2. Fill form
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group_name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group_header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group_footer)
+        # 3. Submit group
+        driver.find_element_by_name("submit").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def close(self):
+        self.driver.quit()
--- test_create_group.py
+++ test_create_group.py
@@ -1,75 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from fixtures.addressbook_app import AddressbookApp
 import unittest
 
 
 class TestCreateGroup(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(5)
-        self.base_url = "http://localhost/addressbook/"
-        self.driver.get(self.base_url)
+        self.app = AddressbookApp()
+        self.app.login(username="admin", password="secret")
 
     def test_create_group(self):
-        driver = self.driver
-        # Login (precondition)
-        self.login()
-        # Open group page
-        self.open_group_page()
-        # Creating group:
-        self.create_group()
-        # Return to group page
-        self.return_to_group_page()
-        # Logout (postcondition)
-        self.logout()
-
-    def return_to_group_page(self):
-        self.driver.find_element_by_link_text("group page").click()
-
-    def create_group(self):
-        driver = self.driver
-        # 1. Initialization group create
-        driver.find_element_by_name("new").click()
-        # 2. Fill form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("new name")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("new header")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("new footer")
-        # 3. Submit group
-        driver.find_element_by_name("submit").click()
-
-    def logout(self):
-        self.driver.find_element_by_css_selector("form[name=logout] > a").click()
-
-    def open_group_page(self):
-        self.driver.find_element_by_css_selector('#nav > ul > li.admin > a').click()
-
-    def login(self):
-        driver = self.driver
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_css_selector('#LoginForm > input[type="submit"]').click()
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+        self.app.open_group_page()
+        self.app.create_group("new name", "new header", "new footer")
+        self.app.return_to_group_page()
 
     def tearDown(self):
-        self.driver.quit()
+        self.app.logout()
+        self.app.close()
 
 
 if __name__ == "__main__":
