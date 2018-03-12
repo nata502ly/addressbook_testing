5db975c05de9601d32c70e1c5d26b06f3c7415ac
Olena Piankova <el.piankova@gmail.com>
2018-03-12 18:12:06 +0200

    Added independant parametrization of group_name, group_header, group_footer

--- test_create_group.py
+++ test_create_group.py
@@ -1,12 +1,14 @@
 import pytest
 
 group_names = ["new name", "1124", ")(&%$%", "Кирилица"]
+group_headers = ["new header", "45636", "!@#@$", "РПСлтдлфыв"]
+group_footers = ["new footer", "@$%%@%", "@$#%", "РПСлтдлфыв"]
 
 
+@pytest.mark.parametrize("group_footer", group_footers)
+@pytest.mark.parametrize("group_header", group_headers)
 @pytest.mark.parametrize("group_name", group_names)
-def test_create_group(app, init_login, group_name):
-    group_header = "new header"
-    group_footer = "new footer"
+def test_create_group(app, init_login, group_name, group_header, group_footer):
     app.open_group_page()
     app.create_group(group_name, group_header, group_footer)
     assert app.message_page.is_this_page()
