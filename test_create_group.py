e2e5e6f561be56922f24bcc7e2f82c00bf127729
Olena Piankova <el.piankova@gmail.com>
2018-03-12 18:08:07 +0200

    Added parametrization of group_name

--- test_create_group.py
+++ test_create_group.py
@@ -1,6 +1,15 @@
-def test_create_group(app, init_login):
+import pytest
+
+group_names = ["new name", "1124", ")(&%$%", "Кирилица"]
+
+
+@pytest.mark.parametrize("group_name", group_names)
+def test_create_group(app, init_login, group_name):
+    group_header = "new header"
+    group_footer = "new footer"
     app.open_group_page()
-    app.create_group("new name", "new header", "new footer")
+    app.create_group(group_name, group_header, group_footer)
     assert app.message_page.is_this_page()
     assert "A new group has been entered into the address book." in app.message_page.message_box.text
     app.return_to_group_page()
+
