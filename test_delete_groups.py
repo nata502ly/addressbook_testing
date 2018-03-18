94a03765f6f0aeb311fc0741fb2302b44061b956
Olena Piankova <el.piankova@gmail.com>
2018-03-18 21:47:39 +0200

    Changed deletion test for random group

--- test_delete_groups.py
+++ test_delete_groups.py
@@ -1,6 +1,10 @@
-def test_delete_first_group(app, init_login):
+import random
+
+
+def test_delete_random_group(app, init_login):
     app.open_group_page()
-    app.delete_group(number=0)
+    random_number = random.randrange(0, len(app.group_view_page.group_checkboxes))
+    app.delete_group(number=random_number)
     assert app.message_page.is_this_page()
     assert "Group has been removed." in app.message_page.message_box.text
     app.return_to_group_page()
