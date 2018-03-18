81b7b138a7dcd7811b34f6a9a4cccd35fb300de8
Olena Piankova <el.piankova@gmail.com>
2018-03-18 21:55:27 +0200

    Add random string data for parametrization

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import json
+import random
+import string
 from fixtures.addressbook_app import AddressbookApp
 from models.group import Group
 
@@ -18,8 +20,22 @@ def init_login(app):
     app.logout()
 
 
+def random_string(max_len):
+    length = random.randrange(1, max_len)
+    symbols = string.ascii_letters + string.digits + string.punctuation + ' '*10
+    result = ""
+    for _ in range(length):
+        symbol = random.choice(symbols)
+        result += symbol
+    return result
+
+
 with open("group_data.json", encoding="utf8") as f:
     group_list = json.load(f)
+
+group_list += [{"name": random_string(25),
+                "header": random_string(25),
+                "footer": random_string(25)} for _ in range(3)]
 
 
 @pytest.fixture(params=group_list, ids=[str(group) for group in group_list])
